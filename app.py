be5172d9de102cd9b9967c2c7607689a675eb29a
totalspastic <65527347+totalspastic@users.noreply.github.com>
2022-11-27 14:28:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-#from flask import Flask, render_template
-from flask import Flask
+from flask import Flask, render_template
+#from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'hello'
-   #return render_template('index.html')
-#if __name__ == '__main__':
-   #app.run()
+   #return 'hello'
+   return render_template('index.html')
+if __name__ == '__main__':
+   app.run()
