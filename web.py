8da311d201e9e91e7bc61830f554d3def55782ce
totalspastic <65527347+totalspastic@users.noreply.github.com>
2022-11-27 11:26:34 +0530

    Update web.py

--- web.py
+++ web.py
@@ -2,9 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-n=int(input("Enter first number:\n"))
-m=int(input("Enter second number:\n"))
 def subtract(a,b):
-  subtract=a-b
-  return subtract
-print("Result: ", subtract(n,m))
+  return(a-b)
+print(subtract(5,3))
